0d092999ec99f3ac3281f2abecf4a8b5684740ac
Clara Chavarette <clarachavarettez@gmail.com>
2026-05-09 17:44:58 -0300

    commit-3

--- heap.py
+++ heap.py
@@ -1,7 +1,18 @@
 import heapq
 import sys
+import logging
 
 filaPacientes = []
+
+
+# configura o arquivo, o nível de log e o formato da mensagem
+logging.basicConfig(
+    filename='registro-atendimentos.log', 
+    level=logging.INFO,
+    format='%(asctime)s - %(levelname)s - %(message)s',
+    datefmt='%d/%m/%Y %H:%M:%S'
+)
+
 
 
 def exibirMenu():
@@ -24,11 +35,14 @@ def exibirMenu():
         case 3: 
             exibirFilaPacientes()
         case 4: 
+            logging.info("Atendimento finalizado.") # Registra log 
+
             print('\n' + '-'*30) #linha separadora
             print('   ATENDIMENTO FINALIZADO')
             print('-'*30) #linha separadora
             sys.exit() # Fecha o programa 
 
+            
 
 
 
@@ -45,6 +59,7 @@ def cadastrarPaciente():
     #o heap ordena a prioridade pelo 1º elemento da tupla, caso o 1º de empate, ele pula para o 2º e assim vai
     heapq.heappush(filaPacientes, (prioridade, -idade, nome))
 
+    logging.info(f"Paciente cadastrado {(prioridade, -idade, nome)}.") # Registra log 
     exibirMenu()
 
 
@@ -56,14 +71,16 @@ def  atenderPaciente():
     print('      ATENDIMENTO')
     print('-'*30) #linha separadora
 
-    if(filaPacientes):
+    if filaPacientes:
         paciente1 = heapq.heappop(filaPacientes)
 
         print(f"Chamando: {paciente1[2].upper()}")
         print(f"Prioridade: {paciente1[0]} | Idade: {-paciente1[1]}")
 
+        logging.info(f"Paciente atendido - {paciente1[2]}, {paciente1[0]}, {paciente1[1]}.") # Registra log     
     else:
         print(f"A fila está vazia no momento.")
+        logging.info(f"fila está vazia no momento.\n") # Registra log 
 
     exibirMenu()
 
@@ -77,8 +94,9 @@ def exibirFilaPacientes():
     #o heapq.nsmallest(parm1, parm2) é nativo da biblioteca, onde o parm1 é a qtd de items da fila que quero que percorra e o parm2 é a fila, o heap que ele vai percorrer
     for p in heapq.nsmallest(len(filaPacientes), filaPacientes):
         print(f"Prioridade: {p[0]} - Idade: {-p[1]} - Nome: {p[2]}")
+        logging.info(f"Fila - Prioridade: {p[0]} - Idade: {-p[1]} - Nome: {p[2]}") # Registra log 
 
-
+   # logging.info(f"Fila: {len(filaPacientes)} pacientes aguardando.") # Registra log       
     exibirMenu()
 
 
